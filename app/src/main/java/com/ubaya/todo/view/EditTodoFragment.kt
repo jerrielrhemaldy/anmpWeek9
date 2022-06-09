0e297b10c52e44e75fd864988f9eb18736286d0c
jerrielrhemaldy <85059290+jerrielrhemaldy@users.noreply.github.com>
2022-06-09 22:45:45 +0700

    tugas w9

---
+++ app/src/main/java/com/ubaya/todo/view/EditTodoFragment.kt
@@ -0,0 +1,77 @@
+package com.ubaya.todo.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.ubaya.todo.R
+import com.ubaya.todo.databinding.FragmentEditTodoBinding
+import com.ubaya.todo.model.Todo
+import com.ubaya.todo.viewmodel.DetailTodoViewModel
+import kotlinx.android.synthetic.main.fragment_create_todo.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EditTodoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EditTodoFragment : Fragment(), RadioClick, TodoSaveChangesClick {
+    private lateinit var viewModel: DetailTodoViewModel
+    private lateinit var dataBinding:FragmentEditTodoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtJudulTodo.text = "Edit Todo"
+        btnAdd.text = "Save Changes"
+        viewModel = ViewModelProvider(this).get(DetailTodoViewModel::class.java)
+        val uuid = EditTodoFragmentArgs.fromBundle(requireArguments()).uuid
+        viewModel.fetch(uuid)
+        observeViewModel()
+
+//        btnAdd.setOnClickListener {
+//            val radio =
+//                view.findViewById<RadioButton>(radioGroupPriority.checkedRadioButtonId)
+//            viewModel.update(txtTitle.text.toString(), txtNotes.text.toString(),
+//                radio.tag.toString().toInt(), uuid)
+//            Toast.makeText(view.context, "Todo updated", Toast.LENGTH_SHORT).show()
+//            Navigation.findNavController(it).popBackStack()
+//        }
+    }
+
+    fun observeViewModel() {
+        viewModel.todoLD.observe(viewLifecycleOwner, Observer {
+            dataBinding.todo = it
+//            when (it.priority) {
+//                1 -> radioLow.isChecked = true
+//                2 -> radioMedium.isChecked = true
+//                else -> radioHigh.isChecked = true
+//            }
+//            txtTitle.setText(it.title)
+//            txtNotes.setText(it.notes)
+        })
+    }
+
+    override fun onRadioClick(v: View, priority: Int, obj: Todo) {
+        obj.priority = priority
+    }
+
+    override fun onTodoSaveChangesClick(v: View, obj: Todo) {
+        viewModel.update(obj.title, obj.notes, obj.priority, obj.uuid)
+        Toast.makeText(v.context, "Todo Updated", Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/ubaya/todo/view/TodoListAdapter.kt
@@ -0,0 +1,69 @@
+package com.ubaya.todo.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.ubaya.todo.R
+import com.ubaya.todo.databinding.TodoItemLayoutBinding
+import com.ubaya.todo.model.Todo
+import kotlinx.android.synthetic.main.todo_item_layout.view.*
+
+class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick : (Todo) -> Unit)
+    :RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>(),TodoCheckedChangeListener, TodoEditClick {
+
+    class TodoViewHolder(var view:TodoItemLayoutBinding): RecyclerView.ViewHolder(view.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<TodoItemLayoutBinding>(inflater, R.layout.todo_item_layout, parent, false)
+
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.view.todo = todoList[position]
+        holder.view.listener = this
+//        holder.view.checkTask.setText(todoList[position].title.toString())
+
+//        holder.view.imgEdit.setOnClickListener {
+//            val action =
+//                TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
+//
+//            Navigation.findNavController(it).navigate(action)
+//        }
+
+        holder.view.checkTask.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked == true) {
+                adapterOnClick(todoList[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+
+    fun updateTodoList(newTodoList: List<Todo>) {
+        todoList.clear()
+        todoList.addAll(newTodoList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCheckChanged(cb: CompoundButton, isChecked: Boolean, obj: Todo) {
+        if(isChecked) {
+            adapterOnClick(obj)
+        }
+    }
+
+    override fun onTodoEditClick(v: View) {
+        val uuid = v.tag.toString().toInt()
+        val action = TodoListFragmentDirections.actionEditTodoFragment(uuid)
+
+        Navigation.findNavController(v).navigate(action)
+    }
+
+}
